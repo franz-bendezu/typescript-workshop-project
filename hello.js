23c43af9dd95296b3946da6d88f0522858ed07d1
Franz Bendezu <franzant20@gmail.com>
2022-02-11 16:22:54 -0500

    setup

---
+++ hello.js
@@ -0,0 +1,5 @@
+// Saluda al mundo.
+function saludar(persona, fecha) {
+    console.log("Hola ".concat(persona, ", hoy es ").concat(fecha, "!"));
+}
+saludar("Brendan");
---
+++ hello.ts
@@ -0,0 +1,5 @@
+// Saluda al mundo.
+function saludar(persona, fecha) {
+    console.log(`Hola ${persona}, hoy es ${fecha}!`);
+  }
+  saludar("Brendan");
