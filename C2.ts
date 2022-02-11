4191f6bde10bbf00050a1234aedd381835a6d457
Franz Bendezu <franzant20@gmail.com>
2022-02-11 16:33:20 -0500

    basic types of ts

--- C2.ts
+++ C2.ts
@@ -1,3 +1,15 @@
-const greeting: string = 'Hello';
-const lifeAnswer:number = 42;
-const likeShoes:boolean = false;
+const greeting: string = "Hello";
+const lifeAnswer: number = 42;
+const likeShoes: boolean = false;
+const arrNum: number[] = [1, 2, 3, 4]
+const names: string[] = [
+    "JUAN", "Giro"
+]
+// array with generic
+const fruits: Array<string> = [
+    "apple"
+]
+
+let obj:any = {
+    
+}
