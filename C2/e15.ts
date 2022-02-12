c4b328bf4a79f61738ac65b6fb48436e598cdea4
Franz Bendezu <franzant20@gmail.com>
2022-02-12 12:32:12 -0500

    add final examples

---
+++ C2/e15.ts
@@ -0,0 +1,2 @@
+const x = "hello" as number
+const y = "hello" as unknown as  number
---
+++ C2/e16.ts
@@ -0,0 +1,13 @@
+let changingString = "Hello Word";
+changingString = "Hola mundo";
+
+let changingString2: string;
+
+const constantString = "Hello World";
+
+let constantString2: "Hello World";
+
+let aligment: "left" | "right" | "center";
+aligment = "right";
+
+//let aligment: "left" | "right" | "center";
